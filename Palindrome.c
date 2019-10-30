ec184cd848d0ed58cd7031f8363050556c2a362d
Faizakhurshid04 <57167359+Faizakhurshid04@users.noreply.github.com>
2019-10-30 20:45:26 +0530

    Create Palindrome.c

---
+++ Palindrome.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int n, revInteger = 0, remainder, origInteger;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    origInteger = n;
+    // reversed integer is stored in variable 
+    while( n!=0 )
+    {
+        remainder = n%10;
+        revInteger = revInteger*10 + remainder;
+        n /= 10;
+    }
+    // palindrome if origInteger and revInteger are equal
+    if (origInteger == revInteger)
+        printf("%d is a palindrome.", origInteger);
+    else
+        printf("%d is not a palindrome.", origInteger);
+    
+    return 0;
+}
